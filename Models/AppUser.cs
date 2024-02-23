58f9efdc706282cfdd616f0b06007ec339fb9205
Custodian227 <custodian227@gmail.com>
2024-02-23 18:13:10 +0100

    Initial Commit

---
+++ Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace BalanceCheck.Models
+{
+    public class AppUser:IdentityUser
+    {
+    
+        public List<Income> Incomes { get; set; }
+
+        public List<Expense> Expenses { get; set; }
+
+    }
+}
